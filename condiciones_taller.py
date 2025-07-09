81efa41467a0803f32d4b86e8e68c28c5b633ba1
Yesica Andreina Del Valle Rivera Castillo <SENA@PALDFPCBIFSP063.FormacionSena.RED>
2025-07-09 17:12:52 -0500

    Este es mi taller de condicionales

---
+++ condiciones_taller.py
@@ -0,0 +1,132 @@
+# print("Ejercicio 1")
+
+# num= float(input("Ingrese un número: "))
+
+# if num>0:
+#     print(f"El número {num} es positivo")
+
+# elif num<0: 
+#     print(f"El número {num} es negativo")
+
+# else:
+#     print(f"El número es {num}")
+
+# print("Ejercicio 2")
+
+# a= float(input("Ingrese un número: "))
+# b= float(input("Ingrese un número: "))
+
+# if a<b: 
+#     print(f"{b} es el mayor")
+
+# elif b<a:
+#     print(f"{a} es mayor")
+
+# else: 
+#     print("Los dos números son iguales")
+
+# print("Ejercicio 3")
+
+# num2= float(input("Ingresé un número: "))
+# op= num2%2
+
+# if op==0:
+#     print(f"El número {num2} es par")
+
+# else:
+#     print(f"El número {num2} es impar")
+
+# print("Ejercicio 4")
+
+# num3= float(input("INgrese un número: "))
+
+# if num3>=10 and num3<=20:
+#     print(f"El número {num3} si esta entre 10 y 20")
+
+# else: 
+#     print(f"El número {num3} no esta entre 10 y 20")
+
+# print("Ejercicio 5")
+
+# num4= float(input("Ingrese un número: "))
+# num5= float(input("Ingrese un número: "))
+# num6= float(input("Ingrese un número: "))
+
+# if num4>num5 and num4>num6:
+#     print(f"{num4} es el número mayor")
+
+# elif num5>num4 and num5>num6:
+#     print(f"{num5} es el mayor")
+
+# else:
+#     print(f"{num6} es el mayor")
+
+# print("Ejercicio 6")
+
+# total= float(input("Ingrese su total: "))
+
+# if total>=100:
+#     op2= total-((total*1)/100)
+#     print(f"Su total con el descuento será {op2}")
+# else: 
+#     print("No aplica el descuento para su compra")
+
+# print("Ejercicio 7")
+
+# edad= int(input("Ingresé su edad: "))
+
+# if edad>=18:
+#     print("Puede votar")
+# else:
+#     print("No puede votar")
+
+# print("Ejercicio 8")
+
+# precio= float(input("Ingrese su precio: "))
+# cliente= input("Ingrese el tipo del cliente (VIP o normal): ")
+
+# if cliente=="VIP":
+#     op3= precio-((precio*20)/100)
+#     print(f"Su total es {op3}")
+
+# else:
+#     print(f"Su precio es: {precio}")
+
+# print("Ejercicio 9")
+
+# num7= float(input("Ingrese un número: "))
+# op4= num7%3 
+# op5= num7%5
+
+# if op4==0 and op5==0:
+#     print("Su número es divisible de 3 y 5")
+
+# elif op4==0:
+#     print("Tu número es solo divisible entre 3")
+
+# elif op5==0:
+#     print("Tu número es solo divisible entre 5")
+
+# else:
+#     print("Tu número no es divisible ni entre 3 ni entre 5")
+
+# print("Ejercicio 10")
+
+# num8= float(input("Ingrese un número:"))
+# div= float(input("Ingrese un número para saber si el divisible: "))
+# div2= float(input("Ingrese un número para saber si el divisible: "))
+
+# op6= num8%div
+# op7= num8%div2
+
+# if op6==0 and op7==0:
+#     print(f"Tu número {num8} es divisible entre los dos números")
+
+# elif op6==0:
+#     print(f"Tu número {num8} solo es divisible entre {div}")
+
+# elif op7==0:
+#     print(f"Tu número {num8} solo es divisible entre {div2}")
+
+# else: 
+#     print("No es divisible entre ningun número")
